66e48859291554486c1e3437afacf00c3ac33804
HarbingerOfMe <harbingerofme@gmail.com>
2022-03-23 01:07:16 +0100

    test plugin

---
+++ TestPlugin/Class1.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine.Networking;
+
+namespace TestPlugin
+{
+    [BepInPlugin("testplugin", "test plugin, should throw", "0.0.1")]
+    public class Class1 : BaseUnityPlugin
+    {
+        public static ManualLogSource _Logger;
+
+        public void Awake()
+        {
+            _Logger = Logger;
+            new Class2();
+        }
+    }
+
+    public class Class2 : NetworkBehaviour
+    {
+
+    }
+}
