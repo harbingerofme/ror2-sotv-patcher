df5e3d9ea4dc2ffcbb968c552ce591c8f265b768
HarbingerOfMe <harbingerofme@gmail.com>
2022-03-23 01:07:28 +0100

    util classes

---
+++ SotV-patcher/Logger.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SotV_patcher
+{
+    internal static class Logger
+    {
+        private static readonly ManualLogSource logSource = BepInEx.Logging.Logger.CreateLogSource("SotVPatcher");
+
+        public static void Info(object data) => logSource.LogInfo(data);
+
+        public static void Error(object data) => logSource.LogError(data);
+
+        public static void Warn(object data) => logSource.LogWarning(data);
+
+        public static void Fatal(object data) => logSource.LogFatal(data);
+
+        public static void Message(object data) => logSource.LogMessage(data);
+
+        public static void Debug(object data) => logSource.LogDebug(data);
+    }
+}
---
+++ SotV-patcher/Util.cs
@@ -0,0 +1,21 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SotV_patcher
+{
+    internal static class Util
+    {
+        public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> keyValuePair, out T1 key, out T2 value)
+        {
+            key = keyValuePair.Key;
+            value = keyValuePair.Value;
+        }
+
+        public static string Log(this TypeReference typeRef)
+        {
+            return $"{typeRef.Module.Assembly.Name} : {typeRef.FullName}";
+        }
+    }
+}
