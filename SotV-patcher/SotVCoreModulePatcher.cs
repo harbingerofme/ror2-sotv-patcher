adfdc6b2892de9c997599b55321f12b3ce9655c3
HarbingerOfMe <harbingerofme@gmail.com>
2022-03-24 12:56:45 +0100

    add failed attempt

---
+++ SotV-patcher/SotVCoreModulePatcher.cs
@@ -0,0 +1,31 @@
+﻿using Mono.Cecil;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+//This file is NOT compiled!
+
+namespace SotV_patcher
+{
+    internal class SotVCoreModulePatcher
+    {
+        public static IEnumerable<string> TargetDLLs { get; } = new string[] {"UnityEngine.CoreModule.dll"};
+
+        public static void Patch(AssemblyDefinition assembly) {
+            var inputAssembly = AssemblyDefinition.ReadAssembly(Path.Combine(BepInEx.Paths.ManagedPath, "UnityEngine.InputLegacyModule.dll"));
+            var inputTypeRef = assembly.MainModule.ImportReference(inputAssembly.MainModule.GetType("UnityEngine.Input"));
+
+            var typeForwardRef = assembly.MainModule.ImportReference(typeof(TypeForwardedToAttribute).GetConstructors().First());
+            var typeTypeRef = assembly.MainModule.ImportReference(typeof(System.Type));
+
+
+            var ca = new CustomAttribute(typeForwardRef);
+
+            var arg = new CustomAttributeArgument(typeTypeRef, inputTypeRef);
+            ca.ConstructorArguments.Add(arg);
+
+            assembly.CustomAttributes.Add(ca);
+        }
+    }
+}
