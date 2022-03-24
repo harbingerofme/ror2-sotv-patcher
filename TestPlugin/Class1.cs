c278a4862a479beb1f5b052cea64629afa7edffe
HarbingerOfMe <harbingerofme@gmail.com>
2022-03-24 12:57:35 +0100

    update etstplugin to crash harder

--- TestPlugin/Class1.cs
+++ TestPlugin/Class1.cs
@@ -16,8 +16,27 @@ namespace TestPlugin
         }
     }
 
+    [NetworkSettings]
     public class Class2 : NetworkBehaviour
     {
+        public NetworkAnimator _Animator;
 
+
+        public NetworkAnimator blah(NetworkBehaviour networkBehaviour)
+        {
+            var x = new NetworkClient();
+
+            var y = NetworkError.BadMessage;
+
+            var z = x.connection;
+            var xa = UnityEngine.Input.compass;
+
+            Class1._Logger.LogDebug(x);
+            Class1._Logger.LogDebug(y);
+            Class1._Logger.LogDebug(z);
+            Class1._Logger.LogDebug(xa);
+
+            return null;
+        }
     }
 }
